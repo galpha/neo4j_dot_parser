c90eb860fbe312b886fc72556915cb29293f8f5c
Kevin Gomez <k.gomez@freenet.de>
2014-10-27 11:46:45 +0100

    transformator update

--- src/main/java/Transformator.java
+++ src/main/java/Transformator.java
@@ -14,6 +14,7 @@ import org.neo4j.tooling.GlobalGraphOperations;
  * Transformiert auf neo4j basierende GraphDataSets
  * in .DOT format (ohne subgraph)
  *
+ *
  * @author gomezk
  */
 public class Transformator extends Thread {
